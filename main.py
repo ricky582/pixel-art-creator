9a9b1f16523194194db9e9540e792da5f28ed420
Robert Sim SE2020 <robert.sim.2020@uni.strath.ac.uk>
2023-04-30 18:10:27 +0100

    added capability to mask colour away from the image to simplify (remove lines from paper, etc.)

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import numpy as np
-from skimage import io
+from skimage import io, data
 import matplotlib.pyplot as plt 
 
 if __name__ == "__main__":
     img = io.imread("test.jpg")
-    io.imshow(img)
+    maskStrict = (img > 55).any(-1)
+    maskForgiving = (img > 55).all(-1)
+    height, width, d = img.shape
+    out = np.empty(shape=(height, width, 3), dtype="int")
+    
+    out = img.copy()
+
+    out[(maskStrict == True)] = 255
+    
+           
+    io.imshow(out)
     plt.show()
