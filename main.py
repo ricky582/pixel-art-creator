cf7f877809b5231834627ff6e3f92fc22ed60664
Robert Sim SE2020 <robert.sim.2020@uni.strath.ac.uk>
2023-05-01 16:59:37 +0100

    moved filter work into function and added comments

--- main.py
+++ main.py
@@ -2,17 +2,28 @@ import numpy as np
 from skimage import io, data
 import matplotlib.pyplot as plt 
 
-if __name__ == "__main__":
-    img = io.imread("test.jpg")
-    maskStrict = (img > 55).any(-1)
-    maskForgiving = (img > 55).all(-1)
-    height, width, d = img.shape
-    out = np.empty(shape=(height, width, 3), dtype="int")
-    
+# extract_drawing()
+#  takes an image, and filters all pixels lighter than limit (i.e. RGB value > limit), changing them to be white
+#  img -> input image
+#  limit -> limit to brightness of pixel
+#  strict -> if true pixel is filtered if any of the RBG channels > limit, if false all channels must be > limit
+def extract_drawing(img, limit, strict=True):
+    # create mask
+    if strict:
+        mask = (img > limit).any(-1)
+    else: 
+        mask = (img > limit).all(-1)
+    # copy input image (read only otherwise)
     out = img.copy()
-
-    out[(maskStrict == True)] = 255
+    # turn all pixels to white where mask == True
+    out[mask] = 255
+    return out
     
-           
-    io.imshow(out)
+if __name__ == "__main__":
+    # read image from file
+    img = io.imread("test.jpg")
+    # create filtered image
+    filtered = extract_drawing(img, 47)
+    # show new image
+    io.imshow(filtered)
     plt.show()
