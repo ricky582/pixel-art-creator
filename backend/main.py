8f042686bb31c67ae682cec04724a1594bdfcb8e
Robert Sim SE2020 <robbiesim3@gmail.com>
2024-08-19 20:34:53 +0100

    dummy endpoint for returning images

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,8 @@
-from fastapi import FastAPI
+
+import io
+from skimage import io as sk_io
+from imageio import v3 as iio
+from fastapi import FastAPI, Response
 
 app = FastAPI()
 
@@ -6,3 +10,13 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/return-image-test/")
+async def image_test():
+    chosenImg = sk_io.imread("test.jpg")
+    with io.BytesIO() as buf:
+        iio.imwrite(buf, chosenImg, plugin="pillow", format="JPEG")
+        im_bytes = buf.getvalue()
+        
+    headers = {'Content-Disposition': 'inline; filename="test.jpeg"'}
+    return Response(im_bytes, headers=headers, media_type='image/jpeg')
